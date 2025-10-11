a450c8f862a614c5e013cc3101e9a31c321cfd68
jeremiah9020 <jeremiah9020@gmail.com>
2025-10-11 14:59:40 -0700

    Added youtube

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,7 +12,10 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export class FooterComponent {
   instagramIcon = faInstagram
   emailIcon = faEnvelope
+  youtubeIcon = faYoutube
 
   public email = 'lillian.berdean@gmail.com'
   public instagram = 'lillianlynn.art'
+  public youtube = 'lillianlynnart'
+
 }
